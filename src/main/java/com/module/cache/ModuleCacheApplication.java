9a193a4df8fcf1b1bfb10c4943456e451ca98962
qwer9052 <qwer9052@gmail.com>
2022-11-22 21:28:09 +0900

    [Add] cache module init

---
+++ src/main/java/com/module/cache/ModuleCacheApplication.java
@@ -0,0 +1,13 @@
+package com.module.cache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuleCacheApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModuleCacheApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/module/cache/ModuleCacheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.module.cache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuleCacheApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
